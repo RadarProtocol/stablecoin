40e6059642b35c54a0898bdf57a103326b22b694
RedaOps <tudor.posta@live.com>
2022-03-02 18:23:41 +0200

    Finished Stablecoin Tests

--- test/TheStableMoney.test.ts
+++ test/TheStableMoney.test.ts
@@ -113,8 +113,117 @@ describe("Stablecoin", () => {
 
         const bal = await stablecoin.balanceOf(otherAddress1.address);
         expect(bal).to.eq(amount);
+
+        const ts = await stablecoin.totalSupply();
+        expect(ts).to.eq(amount);
     });
-    it.skip("Burning");
-    it.skip("Ownership Transfer");
-    it.skip("ERC20 Functionality");
+    it("Burning", async () => {
+        const {
+            stablecoin,
+            otherAddress1
+        } = await snapshot();
+
+        const amount = ethers.utils.parseEther("1");
+
+        await stablecoin.mint(otherAddress1.address, amount);
+
+        await expect(stablecoin.burn(amount)).to.be.revertedWith(
+            "ERC20: burn amount exceeds balance"
+        );
+
+        await stablecoin.connect(otherAddress1).burn(amount);
+
+        const bs = await stablecoin.balanceOf(otherAddress1.address);
+        const ts = await stablecoin.totalSupply();
+        expect(bs).to.eq(ts).to.eq(0);
+    });
+    it("Ownership Transfer", async () => {
+        const {
+            stablecoin,
+            deployer,
+            otherAddress1
+        } = await snapshot();
+
+        await expect(stablecoin.connect(otherAddress1).transferOwnership(otherAddress1.address)).to.be.revertedWith(
+            "Unauthorized"
+        );
+
+        await stablecoin.connect(deployer).transferOwnership(otherAddress1.address);
+
+        const getPendingOwnerCall = await stablecoin.pendingOwner();
+        expect(getPendingOwnerCall).to.equal(otherAddress1.address);
+
+        await expect(stablecoin.connect(deployer).claimOwnership()).to.be.revertedWith(
+            "Unauthorized"
+        );
+
+        const tx = await stablecoin.connect(otherAddress1).claimOwnership();
+        const receipt = await tx.wait();
+
+        const event = receipt.events![0];
+
+        expect(event.event).to.eq('OwnershipTransferred');
+        expect(event.args.oldOwner).to.eq(deployer.address);
+        expect(event.args.newOwner).to.eq(otherAddress1.address);
+
+        const getPendingOwnerCall2 = await stablecoin.pendingOwner();
+        const getOwnerCall = await stablecoin.owner();
+        expect(getPendingOwnerCall2).to.equal(ethers.constants.AddressZero);
+        expect(getOwnerCall).to.equal(otherAddress1.address);
+
+        await expect(stablecoin.connect(deployer).transferOwnership(otherAddress1.address)).to.be.revertedWith(
+            "Unauthorized"
+        );
+
+        await stablecoin.connect(otherAddress1).transferOwnership(deployer.address);
+    });
+    it("ERC20 Functionality", async () => {
+        const {
+            deployer,
+            otherAddress1,
+            otherAddress2,
+            stablecoin
+        } = await snapshot();
+
+        const amount = ethers.utils.parseEther('100000000');
+        await stablecoin.mint(deployer.address, amount);
+
+        const bs1 = await stablecoin.balanceOf(deployer.address);
+        const ts1 = await stablecoin.totalSupply();
+        expect(bs1).to.eq(ts1).to.eq(amount);
+
+        await stablecoin.transfer(otherAddress1.address, amount);
+        const bs2 = await stablecoin.balanceOf(deployer.address);
+        const bs3 = await stablecoin.balanceOf(otherAddress1.address);
+        const ts2 = await stablecoin.totalSupply();
+        expect(bs3).to.eq(ts2).to.eq(amount);
+        expect(bs2).to.eq(0);
+
+        await stablecoin.connect(otherAddress1).approve(otherAddress2.address, amount);
+
+        const all1 = await stablecoin.allowance(otherAddress1.address, otherAddress2.address);
+        expect(all1).to.eq(amount);
+
+        await stablecoin.connect(otherAddress2).transferFrom(otherAddress1.address, otherAddress2.address, amount);
+
+        const all2 = await stablecoin.allowance(otherAddress1.address, otherAddress2.address);
+        expect(all2).to.eq(0);
+
+        const bs4 = await stablecoin.balanceOf(otherAddress1.address);
+        const bs5 = await stablecoin.balanceOf(otherAddress2.address);
+        expect(bs4).to.eq(0);
+        expect(bs5).to.eq(amount);
+
+        const finalTS = await stablecoin.totalSupply();
+        expect(finalTS).to.eq(amount);
+
+        await expect(stablecoin.connect(deployer).transferFrom(otherAddress2.address, deployer.address, amount)).to.be.revertedWith(
+            "ERC20: insufficient allowance"
+        );
+
+        await stablecoin.connect(otherAddress2).burn(amount);
+
+        const ts0 = await stablecoin.totalSupply();
+        expect(ts0).to.eq(0);
+    });
 });
