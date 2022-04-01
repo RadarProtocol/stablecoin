4053444869a72afe710505555ea3d37a277e4b84
RedaOps <tudor.posta@live.com>
2022-04-01 20:15:35 +0300

    Implemented pay loan with collateral

--- contracts/core/LendingPool.sol
+++ contracts/core/LendingPool.sol
@@ -263,6 +263,58 @@ contract LendingPair is ReentrancyGuard {
         require(_userSafe(msg.sender), "User not safe");
     }
 
+    function hookedRepayAndWithdraw(
+        uint256 _directRepayAmount,
+        uint256 _withdrawAmount,
+        bytes calldata _swapData
+    ) external {
+        // 1. Withdraw and send direct repay
+        if (_directRepayAmount != 0) {
+            IERC20(lendAsset).safeTransferFrom(msg.sender, swapper, _directRepayAmount);
+        }
+
+        uint256 _before = ILickHitter(yieldVault).balanceOf(lendAsset, address(this));
+        _withdraw(_withdrawAmount, swapper);
+
+        // 2. Swap for lendAsset
+        ISwapper(swapper).repayHook(
+            collateral,
+            _swapData
+        );
+
+        // 3. Repay loan (use before/after calculation)
+        uint256 _after = ILickHitter(yieldVault).balanceOf(lendAsset, address(this));
+        uint256 _repayAmount = ILickHitter(yieldVault).convertShares(lendAsset, (_after - _before), 0);
+        require( _repayAmount > 0, "Repay 0");
+
+        uint256 _fee = (_repayAmount * EXIT_FEE) / GENERAL_DIVISOR;
+        accumulatedFees = accumulatedFees + _fee;
+        uint256 _userRepayAmount = _repayAmount - _fee;
+
+        if (_userRepayAmount > borrows[msg.sender]) {
+            // Dust will be left, beucase we are
+            // trying to repay more than the
+            // actual loan itself, so we will
+            // be sending the leftover borrowed
+            // assets to the user's LickHitter
+            // account
+            uint256 _dustLeft = _userRepayAmount - borrows[msg.sender];
+            _userRepayAmount = borrows[msg.sender];
+            totalBorrowed = totalBorrowed - _userRepayAmount;
+            borrows[msg.sender] = 0;
+
+            // Convert to shares and send
+            _dustLeft = ILickHitter(yieldVault).convertShares(lendAsset, 0, _dustLeft);
+            ILickHitter(yieldVault).transferShares(lendAsset, msg.sender, _dustLeft);
+        } else {
+            totalBorrowed = totalBorrowed - _userRepayAmount;
+            borrows[msg.sender] = borrows[msg.sender] - _userRepayAmount;
+        }
+        emit LoanRepaid(msg.sender, _userRepayAmount, msg.sender);
+
+        require(_userSafe(msg.sender), "User not safe");
+    }
+
 
     // Not-reentrant for extra safety
     // The `_liquidator` must implement the
