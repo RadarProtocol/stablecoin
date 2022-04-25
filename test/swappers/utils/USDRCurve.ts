667b4afc65eba83a392ec15282785f2a272d2fef
RedaOps <tudor.posta@live.com>
2022-04-25 20:05:46 +0300

    Benqi sAVAX Swapper tests

--- test/swappers/utils/USDRCurve.ts
+++ test/swappers/utils/USDRCurve.ts
@@ -385,3 +385,17 @@ export const setavaxUSDTTokenBalance = async (
         false
     );
 }
+
+export const setavaxSAVAXTokenBalance = async (
+    receiver: SignerWithAddress,
+    amount: BigNumberish
+) => {
+    await manipulateLocalERC20Balance(
+        receiver,
+        "0x2b2C81e08f1Af8835a78Bb2A90AE924ACE0eA4bE",
+        203, // Slot for SAVAX (AVAX) token
+        receiver.address,
+        amount,
+        false
+    );
+}
