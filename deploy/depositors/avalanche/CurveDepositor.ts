e073fed0455e1c427e1f0bdb20ec09b86b329948
RedaOps <tudor.posta@live.com>
2022-05-10 03:18:00 +0300

    CurveDepositor deployment script avalanche

---
+++ deploy/depositors/avalanche/CurveDepositor.ts
@@ -0,0 +1,31 @@
+import { ethers } from 'ethers';
+import { DeployFunction } from 'hardhat-deploy/types';
+
+import { loadConfig } from '../../utils/config';
+
+const fn: DeployFunction = async function (hre) {
+
+    const {
+        deployments: { deploy, get },
+        ethers: { getSigners },
+    } = hre;
+
+  const deployer = (await getSigners())[0];
+  const config = await loadConfig(hre);
+  
+  await deploy('CurveDepositor', {
+      from: deployer.address,
+      log: true,
+      skipIfAlreadyDeployed: true
+  });
+};
+
+fn.tags = ['Core', 'Depositor', 'CurveDepositor'];
+fn.dependencies = ['Config'];
+fn.skip = async (hre) => {
+  // Skip this on non-core deployments or when network isn't avalanche
+  const config = await loadConfig(hre);
+  return config.DEPLOYMENT_TYPE != "CORE" || (config.NETWORK !== 43114 && !config.isDevDeploy );
+};
+
+export default fn;
